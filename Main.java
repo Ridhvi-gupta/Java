c3e8121857c833e69bd841f7c40942925c5bd896
Ridhvi Gupta <ridhvigupta3@gmail.com>
2026-02-08 18:10:57 +0530

    java

--- Main.java
+++ Main.java
@@ -1,12 +1,21 @@
 // First Java program
+
 // public class Main {
 //     public static void main(String[] args) {
 //         System.out.println("Hello World!");
 //     }
-// }
+// }  output is Hello World!
+
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println(args[0]);
+//     }
+//     } is runned from javac Main.java and
+//     java Main 30 , the output is 30
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(args[0]);
+        System.out.println(args[1]);
     }
-}
+} // this is runned from javac Main.java and
+  //   java Main 30 "Kunal", the output is Kunal
